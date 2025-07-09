c64193f7e9d2b3e33a79df7d41d741b67d733ab4
laviniacarol <carolxlavinia@gmail.com>
2025-07-09 16:20:13 -0300

    botão de menu com media queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 // ASSETS
 import Logo from '../../assets/dnc-logo.svg'
 import './Header.css'
 
+//COMPONENTS
+import Button from "../Button/Button"
+
+
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to='/'> <img src={Logo} /> </Link>
-               
-             <nav>
+               <div className="mobile-menu">
+                <Button buttonStyle="secondary" onClick={toggleMenu}>
+                    MENU
+                </Button>
+               </div>
+
+             <nav className={`${isOpen ? 'open' : ''}`}>
+                <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+                    X
+                </Button>
                 <ul className="d-flex">
                 <li><Link to='/'> Home </Link></li>
                 <li><Link to='/about'> About </Link></li>
@@ -22,8 +39,8 @@ function Header() {
                 </ul>
              </nav>
                 </div>
+               </div>
 
-            </div>
         </header>
     );
 }
